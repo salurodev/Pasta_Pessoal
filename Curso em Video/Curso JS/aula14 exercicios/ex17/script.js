00b0271212cd1173685e901131ed63a7dd0c24b9
salurodev <salurodev@gmail.com>
2022-05-24 20:21:13 -0300

    exercicios aula 14

---
+++ Curso em Video/Curso JS/aula14 exercicios/ex17/script.js
@@ -0,0 +1,31 @@
+function contar() {
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('resultado')
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo não pode ser negativo e nem Zero, Vou considerar Passo 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1} tan tan`
+    }
+}
