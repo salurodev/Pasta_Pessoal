96b143f09da31e595fe958e6956ea41cfdb1ca3e
salurodev <salurodev@gmail.com>
2022-05-24 17:50:11 -0300

    aula 13 e 14

---
+++ Curso em Video/Curso JS/aula13/ambiente.js
@@ -0,0 +1,21 @@
+/* executa o teste logico por ultimo */
+
+
+var c = 1
+do {
+    console.log(`Isso repetiu ${c} vezes`)
+    c++
+} while (c <= 10)
+
+
+
+
+
+
+
+/* executa o teste logico primeiro */
+var c = 1
+while (c <= 10) {
+    console.log(`Isso repetiu ${c} vezes`)
+    c+=1
+}
---
+++ Curso em Video/Curso JS/aula14/ambiente.js
@@ -0,0 +1,12 @@
+/* var c = 1
+while (c <= 4) {
+    console.log(`repetiu ${c} vezes`)
+    c++
+} */
+
+
+console.log('Vai começar em "5"')
+for(var c = 1;c <= 5;c++) {
+    console.log(`repetiu ${c}`)
+}
+console.log('FIM')
