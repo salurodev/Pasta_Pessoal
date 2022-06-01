24da4e51ad00335d21945005bf0e755bff7913ea
salurodev <salurodev@gmail.com>
2022-06-01 16:22:14 -0300

    conclusão exercicio Ampli

---
+++ Ampli/prog04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float salario_bruto, desc_inss, desc_ir, salario_liquido;
+
+    printf("\n\n Cálculo de Slário com desconto do IR e INSS");
+    printf("\n Digite seu salario bruto: ");
+    scanf("%f", &salario_bruto);
+    
+}
